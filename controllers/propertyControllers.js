e30e2afc69e12175a31880a9b5277706826809fa
Praneeth Kumar Reddy <pkrpunreddy@myseneca.ca>
2022-01-25 15:53:15 -0500

    database and conrollers setup for property

--- controllers/propertyControllers.js
+++ controllers/propertyControllers.js
@@ -2,7 +2,20 @@ const express = require("express");
 const router = express.Router();
 const propertyService=require("../services/propertyServices.js")
 
+router.get("/",propertyService.getallproperty)
 
+router.get("/types",propertyService.getallpropertytypes)
 
+router.get("/:type",propertyService.getApropertytype)
+
+router.get("/:location",propertyService.getApropertylocation)
+
+router.get("/:value",propertyService.getApropertyvalue)
+
+router.get("/:id",propertyService.getApropertyid)
+
+router.put("/:id",propertyService.updateAproperty)
+
+router.delete("/:id",propertyService.deleteAproperty)
 
 module.exports = router;
--- models/propertyModels.js
+++ models/propertyModels.js
@@ -1,2 +1,40 @@
 const mongoose=require("mongoose");
 const  Schema =mongoose.Schema;
+
+const propertySchema = new Schema({
+    title:
+    {
+        type:String,
+        required:true
+    },
+    "rentalPrice(pernight)":
+    {
+        type:Number,
+        required:true
+    },
+    description:
+    {
+        type:String
+    },
+    type:
+    {
+        type:String,
+        required:true
+    },
+    location:
+    {
+        type:String,
+        required:true
+    },
+    photo:
+    {
+        type:String
+    }
+})
+
+propertySchema.add({ houseRules: [{ type:String }] })
+propertySchema.add({ amenities: [{ type:String }] , required:true })
+
+const property = mongoose.model("property",propertySchema);
+
+module.exports=property;
